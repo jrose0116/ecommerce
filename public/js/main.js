06e2179c13227f0de0ac38ed8cced24319dbc87d
jrose0116 <jrose0116@gmail.com>
2023-05-30 02:05:01 -0400

    Cart clientside

---
+++ public/js/main.js
@@ -0,0 +1,26 @@
+let hoverTime = 300;
+let hoveredElem = false;
+var timeout;
+
+const refresh2 = () => {
+	$.ajax({
+		method: "GET",
+		dataType: "html",
+		credential: "same-origin",
+		data: {},
+		success: (data) => {
+      $("#checkout-elem").html($(data).find("#checkout-elem").html());		},
+		error: (obj, err, errT) => {
+			console.log("Error: ", obj.responseText);
+		},
+	});
+};
+
+document.getElementById('checkout-elem').onmouseover = () => {
+  document.getElementById('nav-elem-checkout').style.display = "flex";
+  clearTimeout(timeout)
+}
+
+document.getElementById('checkout-elem').onmouseout = () => {
+  timeout = setTimeout(()=>{document.getElementById('nav-elem-checkout').style.display = "none"; refresh2()}, hoverTime)
+}
