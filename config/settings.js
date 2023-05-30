3ee8c945e145b88f9ff034341ca531d5425881da
jrose0116 <jrose0116@gmail.com>
2023-05-30 17:11:10 -0400

    Switch to mongo Atlas

--- config/settings.js
+++ config/settings.js
@@ -1,5 +1,8 @@
+import dotenv from "dotenv";
+dotenv.config();
+
 export const mongoConfig = {
   // serverUrl: "mongodb://localhost:27017/",
-  serverUrl: 'mongodb://127.0.0.1:27017/',
+  serverUrl: `mongodb+srv://jrose0116:${process.env.mongoPass}@cluster.nbrlskp.mongodb.net/?retryWrites=true&w=majority`,
   database: "JacobRose_eCommerce",
 };
