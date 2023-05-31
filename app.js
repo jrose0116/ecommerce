8e71bafd512e64b311aaff811855bc19227f7416
jrose0116 <jrose0116@gmail.com>
2023-05-30 20:36:15 -0400

    Update app.js

--- app.js
+++ app.js
@@ -33,6 +33,8 @@ app.set("view engine", "handlebars");
 
 configRoutes(app);
 
-app.listen(process.env.PORT || 3000, () => {
-    console.log("Server running. http://localhost:" + process.env.PORT || 3000)
+let port = process.env.PORT || 3000
+
+app.listen(port, () => {
+    console.log("Server running. http://localhost:" + port)
 })
